4224b3b84b0b263514784e96e802ebe1fa1c4449
Choudhary Reyan Khan <93575436+ReyanChaudhary@users.noreply.github.com>
2022-10-20 01:05:04 +0530

    Create printingsizeofastring.cpp

---
+++ printingsizeofastring.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	string s;
+	cin >> s;
+	int size = s.size();
+	cout << "Size of string is : " << size << endl;
+	return 0;
+}
